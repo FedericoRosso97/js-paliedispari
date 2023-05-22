ca326f9c7d5f49d8b369f92fcbda755f924ffd5c
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-22 16:50:54 +0200

    in funzione, se risultato è pari assegno  false

--- pariDispari/script.js
+++ pariDispari/script.js
@@ -30,9 +30,9 @@ console.log(numeroUtente);
     console.log(numeroPc);
  }
 
- function pariDispari(num1+num2){
-    if((num1+num2)%2==0;){
-        
+ function pariDispari(num1,num2){
+    if((num1+num2)%2==0){
+        return true
     }
  }
 
