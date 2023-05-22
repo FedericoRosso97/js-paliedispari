2bb481e3a27bd503e16c8335f33f9dc2b80018b1
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-22 15:51:23 +0200

    creo funzione isPalindrome

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,3 +1,19 @@
-console.log('ciao')
+function isPalindrome(word){
+    let wordArray=[];
+    wordArray=word.split('');
+    
+    let arrayReverse=wordArray.reverse();
+    
+    wordText=wordArray.toString();
+    reverseText=arrayReverse.toString();
 
-let word=prompt('inserisci una parola');
+    if(word==reverseText){
+        return true;
+    }
+}
+
+let userWord=prompt('inserisci una parola');
+console.log(userWord);
+
+let palindromeCheck=isPalindrome(userWord);
+console.log(palindromeCheck);
